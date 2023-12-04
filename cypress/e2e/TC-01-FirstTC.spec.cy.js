69ca271e9cc4e60d53c175fa1e1b8fb69a231337
LamriL <lamrinx@msn.com>
2023-12-04 10:51:36 +0100

    ...

--- cypress/e2e/TC-01-FirstTC.spec.cy.js
+++ cypress/e2e/TC-01-FirstTC.spec.cy.js
@@ -1,7 +1,8 @@
 describe('First Test Case',function(){
  
     it('Conduit Login',function(){
-        cy.visit('https://react-redux.realworld.io')
+        //cy.visit('https://react-redux.realworld.io')
+        cy.visit('https://react-redux.realworld11.io')
         cy.contains('Sign in').click()
         cy.get('input[type="email"]').type('cypressdemo@gmail.com')
         cy.get('input[type="password"]').type('cypressdemo')
